27bd940b36730ff94e94ec8a3df532c767d4fa76
Sahil Khanolkar <sahilkhanolkar@avegen-00189.lan>
2022-11-21 13:48:37 +0530

    project done

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button} from 'react-native';
+import React, { useEffect, useState} from 'react';
+import axios from "axios";
 
 export default function App() {
+  const REACT_APP_API_URL = 'https://api.kanye.rest'
+  const [quotes, setQuotes] = useState("");
+
+  const getQuote = () => {
+    axios
+    .get(REACT_APP_API_URL)
+    .then(data => setQuotes(data.data.quote))
+    .catch(error => console.log(error));
+    };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.minicontainer}>
+      <Text style={{color : 'white'}}>{quotes}</Text>    
+      <Button
+        onPress={() => {getQuote()}}
+        title="Change Quote"
+        color="#2A3990"
+      />
       <StatusBar style="auto" />
+    </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  minicontainer: {
+    marginHorizontal: 85,
+    marginVertical:320,
+    backgroundColor:"#9C254D",
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10,
+    borderRadius:20,
+    flex: 1, justifyContent: 'center', alignItems: 'center' 
+  },
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex : 1,
+    backgroundColor:"#D23369",
   },
 });
